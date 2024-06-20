8cb54b7c96251b359095646c33d5de702a19816a
Laksheya Pal <46328924+Laksheya@users.noreply.github.com>
2024-06-21 04:20:18 +0530

    Tilte changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "siddanth/portfolio",
+  title: "laksheya/portfolio",
   description: "me",
 };
 
